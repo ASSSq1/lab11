557458b1c05ad86b17d2154ec17216a9c4b04842
ASSSq1 <113017117+ASSSq1@users.noreply.github.com>
2022-10-08 23:21:50 +0300

    Add files via upload

---
+++ lab11/Untitled1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n =33;
+    int *p=&n;
+    printf(Address of "n=  %p\n":,&n);
+    ptr = (int*)malloc(n * sizeof(int));
+    printf("%d",p);
+     printf("%p");
+    cout<<p<<;
+
+    return 0;
+}
